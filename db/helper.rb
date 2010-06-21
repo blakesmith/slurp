1edc00367064a3408e17de80e78ce445f979cb6a
Blake Smith <blakesmith0@gmail.com>
2010-06-21 18:08:38 -0500

    Populate the database with parsed fields

--- db/helper.rb
+++ db/helper.rb
@@ -1,4 +1,5 @@
+require 'rubygems'
 require 'sequel'
-Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://my.db')
+DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://my.db')
 
 Dir.glob(File.join(File.dirname(__FILE__), 'models/*.rb')).each {|r| require r }
--- slurpserver.rb
+++ slurpserver.rb
@@ -1,9 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'db/helper'
 
 post '/link/create' do
   match = params['msg'].match(/:(.*)!.* :(.*)/)
   user = match[1]
   msg = match[2]
-  "#{params['url']} saved with message: #{params['msg']}\nuser: #{user}\nmsg: #{msg}"
+  if match
+    Link.create :user => user, :url => params['url'], :date_added => Time.now, :message => msg
+    "#{params['url']} saved with message: #{params['msg']}\nuser: #{user}\nmsg: #{msg}"
+  else
+    "Post failed"
+  end
 end
