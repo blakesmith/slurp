2b0c6f415c86f0d3a936d7558e85a5520506e355
Blake Smith <blakesmith0@gmail.com>
2010-06-21 16:55:30 -0500

    Parse out user and message when data is posted

--- slurpserver.rb
+++ slurpserver.rb
@@ -2,5 +2,8 @@ require 'rubygems'
 require 'sinatra'
 
 post '/link/create' do
-  "#{params['url']} saved with message: #{params['msg']}"
+  match = params['msg'].match(/:(.*)!.* :(.*)/)
+  user = match[1]
+  msg = match[2]
+  "#{params['url']} saved with message: #{params['msg']}\nuser: #{user}\nmsg: #{msg}"
 end
