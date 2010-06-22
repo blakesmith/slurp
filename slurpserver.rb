43e53ec6cbf52c8f70ddf00929ddbbc0cb810ad0
Blake Smith <blakesmith0@gmail.com>
2010-06-22 00:03:56 -0500

    Heroku stuff

--- slurpserver.rb
+++ slurpserver.rb
@@ -2,6 +2,16 @@ require 'rubygems'
 require 'sinatra'
 require 'db/helper'
 
+begin
+  # Require the preresolved locked set of gems.
+  require ::File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fallback on doing the resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
 get '/' do
   @links = Link.order(:date_added).reverse
   erb :index
