93b3c3ea061fd3c50de781486754663a6acaeccb
Blake Smith <blakesmith0@gmail.com>
2010-06-22 10:20:56 -0500

    Strip the url of white space before fetching title

--- db/models/link.rb
+++ db/models/link.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 class Link < Sequel::Model
 
   def self.fetch_title(url)
-    doc = open(url)
+    doc = open(url.strip)
     if doc.status.first == "200"
       select = Nokogiri::HTML(doc).css('title')
       select ? select.text : "Unknown"
