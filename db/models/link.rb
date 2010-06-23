67946b6db3f429a661a064622cf852a698c1950a
Blake Smith <blakesmith0@gmail.com>
2010-06-23 13:34:08 -0500

    Show a link even if it doesn't have a title

--- db/models/link.rb
+++ db/models/link.rb
@@ -8,7 +8,15 @@ class Link < Sequel::Model
     doc = open(url.strip)
     if doc.status.first == "200"
       select = Nokogiri::HTML(doc).css('title')
-      select ? select.text : "Unknown"
+      select ? select.text : "Link"
+    end
+  end
+
+  def sanitized_title
+    if title.strip.empty?
+      "Link"
+    else
+      title.strip
     end
   end
 end
