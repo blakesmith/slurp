840cc6d80b0cc6f3f67d8bc740d3b35f370850d2
Blake Smith <blakesmith0@gmail.com>
2010-07-08 16:24:29 -0500

    Cleanup some looks and styling

--- db/models/link.rb
+++ db/models/link.rb
@@ -3,13 +3,20 @@ require 'nokogiri'
 require 'open-uri'
 
 class Link < Sequel::Model
-
+  
   def self.fetch_title(url)
     doc = open(url.strip)
     if doc.status.first == "200"
       select = Nokogiri::HTML(doc).css('title')
       select ? select.text : "Link"
     end
+  end
+
+  def shortened_message
+    shortened_length = 60
+    m = message.slice(0..shortened_length)
+    m += "..." if m.length >= shortened_length
+    m
   end
 
   def sanitized_title
